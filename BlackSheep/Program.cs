3eb6f47e3318d64a0bfdb5d3b2bdfe6e4100edb0
Opilane TTHK <opilane@tthk.ee>
2021-11-17 10:17:47 +0200

    BlackSheep

---
+++ BlackSheep/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace BlackSheep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"C:\Users\opilane\Samples\blacksheep.txt";
+            string[] datafromfile = File.ReadAllLines(filepath);
+
+            for (int i = 0; i < datafromfile.Length; i++)
+            {
+                datafromfile[i] = datafromfile[i].Replace('4', 'a');
+                datafromfile[i] = datafromfile[i].Replace('3', 'e');
+                datafromfile[i] = datafromfile[i].Replace('0', 'o');
+                datafromfile[i] = datafromfile[i].Replace('1', 'i');
+            }
+
+            foreach (string line in datafromfile)
+            {
+                Console.WriteLine(line);
+            }
+
+            File.WriteAllLines(filepath, datafromfile);
+        }
+    }
+}
---
+++ RandomQuote/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomQuote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
